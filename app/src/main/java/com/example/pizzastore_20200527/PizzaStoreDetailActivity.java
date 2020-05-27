9025ddfd4106c2851a6ddc8c542b92c6d3850f0b
dy696786 <dy696786@gmail.com>
2020-05-27 13:16:07 +0900

    피자가게에 전화 걸기 기능 추가  - TedPermission 활용

--- app/src/main/java/com/example/pizzastore_20200527/PizzaStoreDetailActivity.java
+++ app/src/main/java/com/example/pizzastore_20200527/PizzaStoreDetailActivity.java
@@ -3,11 +3,20 @@ package com.example.pizzastore_20200527;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.pizzastore_20200527.databinding.ActivityPizzaStoreDetailBinding;
 import com.example.pizzastore_20200527.datas.PizzaStore;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 public class PizzaStoreDetailActivity extends BaseActivity {
 
@@ -25,6 +34,39 @@ public class PizzaStoreDetailActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        binding.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PermissionListener permissionListener = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+                        String phoneNum = binding.phoneNumTxt.getText().toString();
+                        Uri myUri = Uri.parse(String.format("tel:%s",mStore.getPhoneNum()));
+                        Intent myintent = new Intent(Intent.ACTION_CALL,myUri);
+                        startActivity(myintent);
+
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+
+                        Toast.makeText(mContxt, "권한이 거부되어 통화가 불가능합니다.", Toast.LENGTH_SHORT).show();
+
+                    }
+                };
+
+                TedPermission.with(mContxt)
+                        .setPermissionListener(permissionListener)
+                        .setDeniedMessage("거부하면 통화가 불가능함. \n설정에서 권한을 켜주세요.")
+                        .setPermissions(Manifest.permission.CALL_PHONE)
+                        .check();
+
+
+            }
+        });
 
     }
 
